0133e28a0d3f28f758921af948a84b9502d343a2
zhangdongxu <zdxlalala@outlook.com>
2023-02-21 21:10:40 -0500

    implemented plot by features and reference page

--- app/ui.R
+++ app/ui.R
@@ -33,9 +33,9 @@ body <- dashboardBody(
     
     tabItem(tabName = "Home", fluidPage(
       fluidRow(box(width = 15, title = "Introduction", status = "primary",
-                   solidHeader = TRUE, h3("Crime Data for the 5 Boroughs in NYC"),
+                   solidHeader = TRUE, h3("Crime Suspect Features for the Five Boroughs in NYC"),
                    h4("By Srushti Divyesh Sanghavi, Mingrui Shi, Weijie Xia, YiXun Xu, Aubrey Yan, Dongxu Zhang"),
-                   h5("A")
+                   h5("Introduction goes here")
       )
       ),
       fluidRow(box(width = 15, title = "Targeted User", status = "primary", solidHeader=TRUE,
@@ -45,35 +45,66 @@ body <- dashboardBody(
                    h5("The application is divided into 5 separate tabs"),
                    tags$div(tags$ul(
                      tags$li("The", strong("first"), "tab: Map can be clicked for detailed info"),
-                     tags$li("The", strong("second"), "tab: A"),
-                     tags$li("The", strong("third and fourth"), "tab: B"),
-                     tags$li("The", strong("fifth"),"tab: C")
-                     
+                     tags$li("The", strong("second"), "tab: Reference")
                    ))
       ))
     )), # end of homepage
     tabItem(
       tabName = "Map",
-      leafletOutput(outputId = "myMap", width = "100%", height = 750)
-    )
+      leafletOutput(outputId = "myMap", width = "100%", height = 750),
+      #control panel on the left
+      absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                    top = 200, left = "auto", right = "auto", bottom = "auto", width = "200", height = "auto",
+                    tags$h4('Graph by suspect features'), 
+                    tags$br(),
+                    awesomeRadio("feature", 
+                                 label="Features",
+                                 choices =c("Age",
+                                            "Race", 
+                                            "Sex"), 
+                                 selected = "Age",
+                                 status = "warning"),
+                    style = "opacity: 0.70"
+      )
+    ),
+    # ------------------ Reference --------------------------------
+    tabItem(tabName = "Reference", fluidPage( 
+      HTML(
+        "<h2> Data Sources </h2>
+                <h4> <p><li>NYC Crime Suspect Data: <a href='https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Current-Year-To-Date-/5uac-w243'>NYPD Complaint Data</a></li></h4>
+                <h4><li>NYC Geo Data : <a href='https://data.cityofnewyork.us/City-Government/Borough-Boundaries/tqmj-j8zm'> NYC Borough Boundaries </a></li></h4>"
+      ),
+      
+      titlePanel("Acknowledgement  "),
+      
+      HTML(
+        "<p>This application is based on R and shiny app.</p>"
+      ),
+      
+      titlePanel("Contributors"),
+      
+      HTML(
+        " <p>Srushti Divyesh Sanghavi(@columbia.edu) </p>",
+        " <p>Mingrui Shi(@columbia.edu)</p>",
+        " <p>Weijie Xia(@columbia.edu) </p>",
+        " <p>YiXun Xu(@columbia.edu)</p>",
+        " <p>Aubrey Yan(@columbia.edu) </p>",
+        " <p>Dongxu Zhang(dz2510@columbia.edu)</p>")
+    )) # end of tab
   )
 )
 
 shinyUI(
-    dashboardPage(
-      title="Crimes in NYC",
-      skin="purple",
-      dashboardHeader(title=span("Crimes in NYC",style="font-size: 16px")),
-      
-      dashboardSidebar(sidebarMenu(
-        menuItem("Home", tabName = "Home", icon = icon("home")),
-        menuItem("Map", tabName = "Map", icon = icon("map-marker-alt")),
-        selectInput("datacol",
-                    label="Sort by",
-                    selected="age",
-                    choices=c("Age","Sex","Race"),
-                    multiple=FALSE)
-      )),
-      body
-    )
+  dashboardPage(
+    title="Crimes in NYC",
+    skin="purple",
+    dashboardHeader(title=span("Crimes in NYC",style="font-size: 16px")),
+    
+    dashboardSidebar(sidebarMenu(
+      menuItem("Home", tabName = "Home", icon = icon("home")),
+      menuItem("Map", tabName = "Map", icon = icon("map-marker-alt")),
+      menuItem("Reference", tabName = "Reference", icon = icon("fas fa-asterisk"))
+    )),
+    body
+  )
 ) #Shiny UI closing    
